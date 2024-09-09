95794f0e170b688d7f80385b9ebd10f0a6d7d48e
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-09-08 19:14:19 -0600

    Creando Models

---
+++ TaskList/Models/Tarea.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList.Models
+{
+    public class Tarea
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Nombre { get; set; }
+
+        [NotNull]
+        public string Estado { get; set; }
+
+        
+        public string Prioridad { get; set; }
+    }
+}
