1aae67a4f5ef767a4409ca6e98fcfde2858009fd
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-09-08 21:54:33 -0600

    Redireccionando a vista ppal

--- TaskList/App.xaml.cs
+++ TaskList/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace TaskList
+﻿using TaskList.Views;
+
+namespace TaskList
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new TareaMain());
         }
     }
 }
